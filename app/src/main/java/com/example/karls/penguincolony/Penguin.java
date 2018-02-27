763e8dc15e590ab3302f844661ad405c47c6d273
karls <karlschmitt190@gmail.com>
2018-02-27 17:27:38 -0600

    Added Constraints and started working on the methods

---
+++ app/src/main/java/com/example/karls/penguincolony/Penguin.java
@@ -0,0 +1,27 @@
+package com.example.karls.penguincolony;
+
+/**
+ * Created by karls on 27-Feb-18.
+ */
+
+public class Penguin {
+    public double breedRate = .25; //Here for future purposes
+    public static int numOfPingus =20;
+
+
+    public String onClickHunt() {
+        if (numOfPingus <= 20) {
+            return "Not Enough Penguins";
+        }
+        else{
+
+
+        }
+    }
+
+
+
+
+
+
+}
