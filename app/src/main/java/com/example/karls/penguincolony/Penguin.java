f936aeb2266906827ba91d275788d8a033baf4bc
karls <karlschmitt190@gmail.com>
2018-02-27 18:59:52 -0600

    Started working on probability

--- app/src/main/java/com/example/karls/penguincolony/Penguin.java
+++ app/src/main/java/com/example/karls/penguincolony/Penguin.java
@@ -3,6 +3,8 @@ package com.example.karls.penguincolony;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Random;
+
 
 /**
  * Created by karls on 27-Feb-18.
@@ -10,10 +12,25 @@ import android.widget.TextView;
 
 public class Penguin extends GamePage {
     public double breedRate = .25; //Here for future purposes
+
     public int numOfPingus =20;
 
     public void onHuntClick(View view){
         TextView textView = (TextView)findViewById(R.id.textView1);
         textView.setText(numOfPingus + "Pingus");
+        Random rand = new Random();
+        int  probability = rand.nextInt(100) + 1;
+
+        numOfPingus = numOfPingus-2;
+        int battlePingus = 2;
+        if (numOfPingus<=2){
+        }
+        else{
+           if (probability<20){
+               battlePingus - 2;
+           }
+
+
+        }
     }
 }
