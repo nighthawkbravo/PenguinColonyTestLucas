24dc9e7391fdc6cfb13202bd7df371b95c2e8acc
karls <karlschmitt190@gmail.com>
2018-02-27 20:35:30 -0600

    Started working on probability

--- app/src/main/java/com/example/karls/penguincolony/Penguin.java
+++ app/src/main/java/com/example/karls/penguincolony/Penguin.java
@@ -15,12 +15,11 @@ public class Penguin extends GamePage {
 
     public int numOfPingus =20;
 
-    public void onHuntClick(View view){
+    public String onHuntClick(View view){
         TextView textView = (TextView)findViewById(R.id.textView1);
-        textView.setText(numOfPingus + "Pingus");
+
         Random rand = new Random();
         int  probability = rand.nextInt(100) + 1;
-
         numOfPingus = numOfPingus-2;
         int battlePingus = 2;
         if (numOfPingus<=2){
@@ -32,6 +31,10 @@ public class Penguin extends GamePage {
             if (probability<20){
                 battlePingus = battlePingus - 1;
             }
+            numOfPingus = numOfPingus+battlePingus;
+            numOfPingus = numOfPingus+battlePingus-5; //Just a practice
+            textView.setText(numOfPingus + "Pingus");
         }
+        return "numOfPingus";
     }
 }
