d4325f87b1c4f8952a6c4f612af81e034bb4b51d
karls <karlschmitt190@gmail.com>
2018-02-27 18:39:31 -0600

    Added a partial method, still needs to be worked on

--- app/src/main/java/com/example/karls/penguincolony/GamePage.java
+++ app/src/main/java/com/example/karls/penguincolony/GamePage.java
@@ -12,10 +12,6 @@ public class GamePage extends AppCompatActivity {
         setContentView(R.layout.activity_game_page);
 
     }
-
-
-
-
     }
 
 
--- app/src/main/java/com/example/karls/penguincolony/Penguin.java
+++ app/src/main/java/com/example/karls/penguincolony/Penguin.java
@@ -1,27 +1,19 @@
 package com.example.karls.penguincolony;
 
+import android.view.View;
+import android.widget.TextView;
+
+
 /**
  * Created by karls on 27-Feb-18.
  */
 
-public class Penguin {
+public class Penguin extends GamePage {
     public double breedRate = .25; //Here for future purposes
-    public static int numOfPingus =20;
+    public int numOfPingus =20;
 
-
-    public String onClickHunt() {
-        if (numOfPingus <= 20) {
-            return "Not Enough Penguins";
-        }
-        else{
-
-
-        }
+    public void onHuntClick(View view){
+        TextView textView = (TextView)findViewById(R.id.textView1);
+        textView.setText(numOfPingus + "Pingus");
     }
-
-
-
-
-
-
 }
